501be87d4d77439e996157d1b1bf0335db592bbe
Ryan Smith <this.ryansmith@gmail.com>
2009-02-21 00:45:18 -0600

    updated the views and added some moer JS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,13 @@ module ApplicationHelper
   # and should return
   #=>
   def get_css
-    ['std','form','list','jquery.autocomplete']
+    ['std','form','list','jquery.autocomplete','jquery.lavalamp']
   end
   def get_js
     [ :defaults, 
+      'jquery.easing.min.js',
+      'jquery.lavalamp.js',
+      'jquery.corners.js',
       'autocomplete/jquery.autocomplete.js',
       'autocomplete/jquery.autocomplete.min.js',
       'autocomplete/jquery.autocomplete.pack.js']
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
 
-    $("input#user_search").autocomplete("auto_complete_for_user_login")
-	
-//	$("#update_permissions").submitWithAjax();
+    $("input#user_search").autocomplete("auto_complete_for_user_login");
+	$('#user_bar').corners();
+	$('#center').corners("10px");
+	$('#left').corners("10px");
+
+	$("#user_nav").lavaLamp({
+        fx: "backout", 
+        speed: 700,
+		homeTop:-1,
+		homeLeft:-1,
+        click: function(event, menuItem) {
+            return true;
+        }
+    });
 	
 });
 //end document
